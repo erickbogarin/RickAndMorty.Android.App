3ac849280c92c439043f7c57e5a4cf3a2e4c4ac0
Erick Bogarin <erick.bogarin@gmail.com>
2025-05-12 22:11:17 -0300

    feat: Adicionar teste de UI para verificar a visibilidade do chip "All"

--- app/src/androidTest/java/com/example/rickandmorty/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/rickandmorty/MainActivityTest.kt
@@ -2,7 +2,6 @@ package com.example.rickandmorty
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.example.rickandmorty.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
